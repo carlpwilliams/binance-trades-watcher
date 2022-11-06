e803cd3fbd0ba9cbc8683f92fae79ed8677865b7
Carl Williams <carl@carlpwilliams.com>
2022-11-06 16:56:39 +0000

    Added total running averages

--- index.js
+++ index.js
@@ -14,38 +14,97 @@ let sellCount = 0;
 let sellTotal = 0;
 let tradeSecond = 0;
 console.info(chalk.blue('starting'))
+let sellValues = []
+let buyValues = []
+let totalBuys = 0;
+let totalSells = 0;
+let maBuys = 0;
+let maSells = 0;
+let epochTotals = {
+    "minute": { "buy": 0, start: new Date(), "sell": 0 },
+    "5minute": { "buy": 0, start: new Date(), "sell": 0 },
+    "15minute": { "buy": 0, start: new Date(), "sell": 0 },
+    "1hr": { "buy": 0, start: new Date(), "sell": 0 }
+};
+
+const timeSince = (date) => {
+    var diff = Math.abs(new Date(date) - new Date()) / 1000;
+    // console.info(diff);
+    return diff;
+
+}
+console.clear();
 binance.websockets.trades(['ETHUSDT'], (trades) => {
 
     let { e: eventType, E: eventTime, s: symbol, p: price, q: quantity, m: maker, a: tradeId } = trades;
-    const theMoment = moment(eventTime).format('mm:ss');
+    
+    let epochSummary = ``;
+    epochSummary = epochSummary + `1m -> ${chalk.green(Math.ceil(epochTotals.minute.buy))} - ${chalk.red(Math.ceil(epochTotals.minute.sell))}`.padEnd(30)
+    if (timeSince(epochTotals.minute.start) > 60) {
+        epochTotals.minute = { "buy": 0, start: new Date(), "sell": 0 };
+    }
+
+    epochSummary = epochSummary.padEnd(45) + `5m -> ${chalk.green(Math.ceil(epochTotals['5minute'].buy))} - ${chalk.red(Math.ceil(epochTotals['5minute'].sell))}`.padEnd(30)
+    if (timeSince(epochTotals['5minute'].start) > 300) {
+        epochTotals['5minute'] = { "buy": 0, start: new Date(), "sell": 0 };
+    }
+
+    epochSummary = epochSummary.padEnd(90) + `15m -> ${chalk.green(Math.ceil(epochTotals['15minute'].buy))} - ${chalk.red(Math.ceil(epochTotals['15minute'].sell))}`.padEnd(30)
+    if (timeSince(epochTotals['15minute'].start) > 900) {
+        epochTotals['15minute'] = { "buy": 0, start: new Date(), "sell": 0 };
+    }
 
     if (!maker) {
+        buyValues.push(quantity);
+        buyValues = buyValues.reverse().slice(0, 1000).reverse();
+        totalBuys = buyValues.reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
+
+        epochTotals.minute.buy = epochTotals.minute.buy + parseFloat(quantity);
+        epochTotals['5minute'].buy = epochTotals['5minute'].buy + parseFloat(quantity);
+        epochTotals['15minute'].buy = epochTotals['15minute'].buy + parseFloat(quantity);
+
+        buyTotal = buyTotal + parseFloat(quantity);
+        maBuys = Math.ceil(totalBuys / buyValues.length);
         buyCount = buyCount + 1;
-        buyTotal = buyTotal + parseInt(quantity);
         sellCount = 0;
 
         sellTotal = 0;
 
     }
     else {
-        sellCount = sellCount + 1;
-        sellTotal = sellTotal + parseInt(quantity);
+        sellValues.push(quantity);
+        sellValues = sellValues.reverse().slice(0, 100).reverse();
+        totalSells = sellValues.reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
+
+
+
+        epochTotals.minute.sell = epochTotals.minute.sell + parseFloat(quantity);
+        epochTotals['5minute'].sell = epochTotals['5minute'].sell + parseFloat(quantity);
+        epochTotals['15minute'].sell = epochTotals['15minute'].sell + parseFloat(quantity);
+
+        sellCount = parseInt(sellCount) + 1;
+        sellTotal = sellTotal + parseFloat(quantity);
+        maSells = Math.ceil(totalSells / sellCount);
+
         buyCount = 0
         buyTotal = 0;
     }
-    let message = `${maker ? sellCount.toString().padEnd(3) + ' Sell'.padEnd(7) : buyCount.toString().padEnd(3) + ' Buy'.padEnd(7)}: ${quantity.padEnd(12)}  @ ${price.padEnd(10)}. ${maker ? sellTotal : buyTotal}`;
+
+    const guage = chalk.green("".padEnd(maBuys, "#")) + "|" + chalk.red("".padEnd(maSells, "#"))
+    let message = `${maker ? sellCount.toString().padEnd(3) : buyCount.toString().padEnd(3)}: ${quantity.padEnd(12)}  @ ${price.padEnd(10)}. ${maker ? Math.round(sellTotal * 100) / 100 : Math.round(buyTotal * 100) / 100}`//.padEnd(100) //+ `${guage}`;
 
     if (maker) {
         message = chalk.red(message)
     }
     else { message = chalk.green(message); }
     if (quantity > 10) {
-        if (quantity > 10) {
-            message = chalk.bold(message)
-        }
-
+        message = chalk.bold(message)
     }
-    console.info(message);
+    if (quantity > 50) {
+        message = chalk.underline(message);
+    }
+    let output = `${message.padEnd(60)} ${epochSummary}`
+    console.info(output);
 
     // console.info(moment(eventTime).format('mm:ss'))
     // console.table(testTable)
